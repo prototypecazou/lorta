c1986842ba9cab92983c76e224509e87c96981b9
Prototypecazou <heycasimir82@hotmail.fr>
2025-03-18 18:18:36 +0100

    Second commit

---
+++ public/js/scrollTrigger.js
@@ -0,0 +1,162 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".word-title-first-section", {
+    y: 0,
+    duration: 1.8,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".first-section", // L'élément déclencheur
+        start: "top 70%", // Déclenchement quand l'élément est à 80% de la fenêtre
+        end: "top 30%", // Optionnel : fin de l'effet
+        toggleActions: "play none none none", // Une seule fois
+    }
+});
+
+gsap.to(".visual-image-first-section", {
+    scaleX: 0,
+    duration: 1.8,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".first-section", // L'élément déclencheur
+        start: "top 70%", // Déclenchement quand l'élément est à 80% de la fenêtre
+        end: "top 30%", // Optionnel : fin de l'effet
+        toggleActions: "play none none none", // Une seule fois
+    }
+});
+
+gsap.to(".first-text-content-right >div:first-child > h3", {
+    y: 0,
+    duration: 1.8,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".first-section", // L'élément déclencheur
+        start: "top 70%", // Déclenchement quand l'élément est à 80% de la fenêtre
+        end: "top 30%", // Optionnel : fin de l'effet
+        toggleActions: "play none none none", // Une seule fois
+    }
+});
+
+
+function splitLines(element,nameOfClass) {
+    const words = element.innerHTML.split(" ");
+    element.innerHTML = words.map(word => `
+        <span class="content-word-paragraphe"><span class=${nameOfClass}>${word}</span></span>`).join(" ");
+}
+
+const paragraph = document.querySelector(".paragraphe-first-section");
+splitLines(paragraph,'word-paragraphe-first-section');
+
+const paragraph2 = document.querySelector(".paragraphe-second-section");
+splitLines(paragraph2,'word-paragraphe-second-section');
+
+gsap.to(".word-paragraphe-first-section", {
+    y: 0,
+    duration: 1.8,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".first-section", // L'élément déclencheur
+        start: "top 70%", // Déclenchement quand l'élément est à 80% de la fenêtre
+        end: "top 30%", // Optionnel : fin de l'effet
+        toggleActions: "play none none none", // Une seule fois
+    }
+});
+
+
+gsap.to(".word-aside-first-section > span", {
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".first-section", // L'élément déclencheur
+        start: "top 15%", // Déclenchement quand l'élément est à 80% de la fenêtre
+        end: "top 0", // Optionnel : fin de l'effet
+        toggleActions: "play none none none", // Une seule fois
+    }
+});
+
+gsap.to(".line", {
+    scaleX: 1,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".first-section", // L'élément déclencheur
+        start: "top 15%", // Déclenchement quand l'élément est à 80% de la fenêtre
+        end: "top 0", // Optionnel : fin de l'effet
+        toggleActions: "play none none none", // Une seule fois
+    }
+});
+
+gsap.utils.toArray(".img-gallery").forEach(img => {
+    gsap.to(img,
+
+        {
+            scale: 0,  // Arrivée : image normale et visible
+            scrollTrigger: {
+                trigger: img, // Déclenchement par l’image
+                start: "top -5%", // Débute quand l’image entre dans la fenêtre
+                end: "top -50%", // Se termine quand l’image arrive en haut
+                scrub: true, // L’animation suit le scroll
+            }
+        }
+    );
+});
+
+gsap.to(".visual-image-second-section", {
+    scaleX: 0,
+    duration: 1.8,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".second-section", // L'élément déclencheur
+        start: "top 70%", // Déclenchement quand l'élément est à 80% de la fenêtre
+        end: "top 30%", // Optionnel : fin de l'effet
+        toggleActions: "play none none none", // Une seule fois
+    }
+});
+
+gsap.to(".word-aside-second-section-1 > span,.word-aside-second-section-2 > span,.word-title-second-section", {
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".second-section", // L'élément déclencheur
+        start: "top 30%", // Déclenchement quand l'élément est à 80% de la fenêtre
+        end: "top 0", // Optionnel : fin de l'effet
+        toggleActions: "play none none none", // Une seule fois
+    }
+});
+
+gsap.to(".visual-littleImage-second-section", {
+    scaleX: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".second-section", // L'élément déclencheur
+        start: "top 70%", // Déclenchement quand l'élément est à 80% de la fenêtre
+        end: "top 30%",
+        toggleActions: "play none none none", // Une seule fois
+    }
+});
+
+gsap.to(".second-text-section > a", {
+    opacity: 1,
+    duration: 1.8,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".second-section", // L'élément déclencheur
+        start: "top 30%", // Déclenchement quand l'élément est à 80% de la fenêtre
+        end: "top 0", // Optionnel : fin de l'effet
+        toggleActions: "play none none none", // Une seule fois
+    }
+});
+
+gsap.to(".word-paragraphe-second-section", {
+    y: 0,
+    duration: 1.8,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".second-section", // L'élément déclencheur
+        start: "top 30%", // Déclenchement quand l'élément est à 80% de la fenêtre
+        end: "top 0", // Optionnel : fin de l'effet
+        toggleActions: "play none none none", // Une seule fois
+    }
+});
--- public/js/smoothScroll.js
+++ public/js/smoothScroll.js
@@ -1,6 +1,6 @@
 
 const lenis = new Lenis({
-  duration: 1.1,  // Durée du scroll (plus grand = plus lent)
+  duration: 1.2,  // Durée du scroll (plus grand = plus lent)
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Courbe d'animation
   smooth: true, // Active le smooth scroll
 });
@@ -11,3 +11,8 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+
+
+  
+
