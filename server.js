b337e873b455bdb3ae364bbabff8db0461bc569e
Prototypecazou <heycasimir82@hotmail.fr>
2025-03-21 18:22:06 +0100

    Four commit

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/contact', (req, res) => {
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
 
